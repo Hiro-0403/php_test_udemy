211ca0254e9134f554f104df5a4942b8c04baa07
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-03 21:42:59 +0900

    if文
    ＝＝＝は、３つを使う方が良い。

--- index2.php
+++ index2.php
@@ -9,5 +9,21 @@ $test_2 = 2;
 // $test_3 = $test_1 % $test_2;
 // よく使う
 
-echo $test_3;
+// echo $test_3;
+
+// if（条件）{
+//   条件が真なら実行
+// }
+
+$height = 90;//数字
+
+var_dump($height);
+
+if($height === "90"){//文字
+  echo "身長は" . $height . "cmです";
+}
+
+// ＝＝は一致
+// ＝＝＝は型も一致
+// できるだけ＝＝＝を使う。
 ?>
