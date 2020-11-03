0a10e0bb462a47a18e2586df5746fce84cffa5d5
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-03 21:25:51 +0900

    四則演算子

---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php
+
+$test_1 = 6;
+$test_2 = 2;
+
+// $test_3 = $test_1 + $test_2;
+// $test_3 = $test_1 - $test_2;
+// $test_3 = $test_1 * $test_2;
+// $test_3 = $test_1 % $test_2;
+// よく使う
+
+echo $test_3;
+?>
