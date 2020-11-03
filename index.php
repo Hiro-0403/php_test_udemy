a5fce574d16a8bae8536bfdfdb986da1bdf40520
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-03 16:52:27 +0900

    配列複数

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!-- 配列 -->
 <?php
-$array =[1,2,3];
+$array_1 =[1,2,3];
 
+$array_2 = [
+  ["赤", "蒼", "黄"],
+  ["緑", "紫", "オレンジ"]
+];
 echo "<pre>";
-var_dump($array);
+var_dump($array_2);
 echo "</pre>";
 
+echo $array_2[1][1];
 ?>
