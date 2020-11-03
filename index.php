ff1828d8098ad8ccbaf2329500e39118b4dc4108
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-03 17:56:08 +0900

    連想配列2

--- index.php
+++ index.php
@@ -18,9 +18,55 @@ $array_member = [
   "hobby" => "サッカー"
 ];
 
-echo $array_member["name"];
+// echo $array_member["name"];
 
 echo "<pre>";
 var_dump($array_member);
+echo "</pre>";
+
+$array_member_2 = [
+  "本田" => [
+    "height" => 180,
+    "hobby" => "サッカー"
+  ],
+  "香川" => [
+    "height" => 175,
+    "hobby" => "野球"
+  ]
+];
+
+// echo $array_member_2["香川"]["height"];
+
+echo "<pre>";
+var_dump($array_member_2);
+echo "</pre>";
+
+$array_member_3 = [
+  "1組" => [
+    "本田" => [
+      "height" => 180,
+      "hobby" => "サッカー"
+    ],
+    "長友" => [
+      "height" => 167,
+      "hobby" => "サッカー"
+    ]
+  ],
+  "2組" => [
+    "香川" => [
+      "height" => 175,
+      "hobby" => "野球"
+    ],
+    "富安" => [
+      "height" => 189,
+      "hobby" => "野球"
+    ]
+  ]
+];
+
+echo $array_member_3["1組"]["本田"]["height"];
+
+echo "<pre>";
+var_dump($array_member_3);
 echo "</pre>";
 ?>
