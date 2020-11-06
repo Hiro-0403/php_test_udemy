be3d5999899ff60d9e9e856b191f4fc1fa0f600f
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-06 23:56:57 +0900

    foreach
    多段階

--- index5.php
+++ index5.php
@@ -18,3 +18,23 @@ foreach ($members as $member) {
 foreach ($members as $key => $value) {
   echo $key . "は" . $value . "です";
 }
+
+$members_2 = [
+　"本田" => [
+  "height" => 170,
+  "hobby" =>　"サッカー"
+　],
+  "香川" => [
+  "height" => 168,
+  "hobby" => "サッカー"
+　]
+];
+
+echo "<br>";
+
+//多段階の配列展開
+foreach($members_2 as $member_1){
+  foreach($member_1 as $member => $value){
+    echo $member . "は" . $value . "です";
+  }
+}
