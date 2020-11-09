3e94dd3ca9d2cbef68d8bc37f46d144e9f67ed1b
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-09 20:50:37 +0900

    ファイルの読み込み
    __DIR__;
    絶対パスを表示
    
    __FILE__;
    ファイルの場所を表示

---
+++ common/common.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$commonVariable = "共通の変数です";
+#変数
+
+function commonTest(){#関数
+  echo "外部ファイルの関数です";
+}
---
+++ index15.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . "/common/common.php";
+
+echo $commonVariable;
+
+echo __FILE__;#ファイルの場所を表示
+
+//__DIR__;
+#絶対パスを表示
+
+commonTest();
