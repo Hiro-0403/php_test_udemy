dbe4bcaec50eefa528a2db5c9cd2b68f6f630d5a
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-11 00:40:40 +0900

    フォーム作成修正

--- index16.php
+++ index16.php
@@ -4,6 +4,14 @@
 #今回は、input.php
 
 $pageFlag = 0;
+
+if(!empty($_POST["btn_confirm"])){
+$pageFlag =1;
+}
+
+if(!empty($_POST["btn_submit"])){
+  $pageFlag =2;
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,12 +20,41 @@ $pageFlag = 0;
   <title>Document</title>
 </head>
 <body>
+
+
+<?php if($pageFlag === 0) : ?>
+
+
 <form method="POST" action="input.php">
 名前
-<input type="text" name="name"></input>
+<input type="text" name="your_name"></input>
 <br>
-<input type="checkbox" name="sports[]" value="野球">野球
-<input type="checkbox" name="sports[]" value="サッカー">サッカー
-<input type="checkbox" name="sports[]" value="">野球
+メールアドレス
+<input type="email" name="email"></input>
+<input type="submit" name="btn_confirm" value="確認する">
+</form>
+
+<?php endif; ?>
+
+<?php if($pageFlag === 1) : ?>
+
+<form method="POST" action="input.php">
+名前
+<?php echo $_POST["your_name"] ; ?>
+<br>
+メールアドレス
+<?php echo $_POST["email"] ; ?>
+<input type="submit" name="btn_confirm" value="送信する">
+<input type="hidden" name="your_name" value=<?php echo $_POST["your_name"] ; ?>>
+<input type="hidden" name="email" value=<?php echo $_POST["email"] ; ?>>
+</form>
+
+<?php endif; ?>
+
+<?php if($pageFlag === 2) : ?>
+完了
+<?php endif; ?>
+
+
 </body>
 </html>
