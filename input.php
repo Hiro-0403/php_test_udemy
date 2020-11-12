181c49533df5b23212c15e21d91dfb77e37d2671
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-12 21:45:07 +0900

    フォーム完了

--- input.php
+++ input.php
@@ -1,16 +1,16 @@
 <?php
 #ファーム作成
-#入力、確認、完了 input.php, confirm.php,thanks.php
+#入力、確認、完了 input.php,confirm.php,thanks.php
 #今回は、input.php
 
 $pageFlag = 0;
 
 if(!empty($_POST["btn_confirm"])){
-$pageFlag =1;
+  $pageFlag = 1;
 }
 
 if(!empty($_POST["btn_submit"])){
-  $pageFlag =2;
+  $pageFlag = 2;
 }
 ?>
 
@@ -23,30 +23,29 @@ if(!empty($_POST["btn_submit"])){
 <?php if($pageFlag === 0) : ?>
 
 <form method="POST" action="input.php">
-名前
-<input type="text" name="your_name"></input>
+氏名
+<input type="text" name="your_name">
 <br>
 メールアドレス
-<input type="email" name="email"></input>
+<input type="email" name="email">
+<br>
 <input type="submit" name="btn_confirm" value="確認する">
-</form>
 
+</form>
 <?php endif; ?>
 
 <?php if($pageFlag === 1) : ?>
-
 <form method="POST" action="input.php">
 名前
-<?php echo $_POST["your_name"] value=""; ?>
+<?php echo $_POST["your_name"] ; ?>
 <br>
 メールアドレス
 <?php echo $_POST["email"] ; ?>
-
-<input type="sumbit" name="back" value="戻る">
+<br>
+<!-- <input type="sumbit" name="back" value="戻る"> -->
 <input type="submit" name="btn_submit" value="送信する">
-<input type="hidden" name="your_name" value=<?php echo $_POST["your_name"] ; ?>>
-<input type="hidden" name="email" value=<?php echo $_POST["email"] ; ?>>
-
+<input type="hidden" name="your_name" value="<?php echo $_POST["your_name"] ; ?>">
+<input type="hidden" name="email" value="<?php echo $_POST["email"] ; ?>">
 </form>
 
 <?php endif; ?>
