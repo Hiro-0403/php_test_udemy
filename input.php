d92b5c97c4aafe26a399d6f38259f32098506269
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-14 18:53:51 +0900

    セキュリティ
    CSRF

--- input.php
+++ input.php
@@ -9,10 +9,11 @@ function h($str)
 #ファーム作成
 #入力、確認、完了 input.php,confirm.php,thanks.php
 #今回は、input.php
-echo "<pre>";
-var_dump($_SESSION);
-echo "</pre>";
-
+if(!empty($_POST)){
+  echo "<pre>";
+  var_dump($_POST);
+  echo "</pre>";
+}
 
 $pageFlag = 0;
 
@@ -42,10 +43,10 @@ $token =$_SESSION["csrfToken"];
 
 <form method="POST" action="input.php">
 氏名
-<input type="text" name="your_name">
+<input type="text" name="your_name" value="<?php if(!empty($_POST["your_name"])){echo $_POST["your_name"] ;} ?>">
 <br>
 メールアドレス
-<input type="email" name="email">
+<input type="email" name="email" value="<?php if(!empty($_POST["email"])){echo $_POST["email"] ;} ?>">
 <br>
 <input type="submit" name="btn_confirm" value="確認する">
 <input type="hidden" name="csrf" value="<?php echo $token?>">
@@ -62,9 +63,9 @@ $token =$_SESSION["csrfToken"];
 メールアドレス
 <?php echo h($_POST["email"]) ; ?>
 <br>
-<!-- <input type="sumbit" name="back" value="戻る"> -->
 <input type="submit" name="back" value="戻る">
 <input type="submit" name="btn_submit" value="送信する">
+<input type="hidden" name="email" value="<?php echo h($_POST["email"]) ; ?>">
 <input type="hidden" name="your_name" value="<?php echo h($_POST["your_name"]) ; ?>">
 <input type="hidden" name="csrf" value="<?php echo h($_POST["csrf"]) ; ?>">
 </form>
@@ -76,7 +77,7 @@ $token =$_SESSION["csrfToken"];
 <?php if($_POST["csrf"] === $_SESSION["csrfToken"]) : ?>
 送信が完了しました。
 
-<?php unset($_SESSION["csrfToken"]) : ?>
+<?php unset($_SESSION["csrfToken"]); ?>
 <?php endif; ?>
 <?php endif; ?>
 
