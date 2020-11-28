9bd600ab580c156a2abd1800437c757dcee8b2ac
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-11-28 19:26:56 +0900

    composerダウンロード、設定

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4d154bbf67c4e483e5c712481b1590b693e555fb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4d154bbf67c4e483e5c712481b1590b693e555fb',
+    ),
+  ),
+);
